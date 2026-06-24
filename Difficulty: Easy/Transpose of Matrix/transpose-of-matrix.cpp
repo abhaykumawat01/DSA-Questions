ca055e0767496c23f966ff9cb7e090bb8c8dd716
ABHAY KUMAWAT <abhaykumawat227@gmail.com>
2026-06-25 01:51:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Transpose of Matrix/transpose-of-matrix.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    vector<vector<int>> transpose(vector<vector<int>>& mat) {
+        // code here
+        for(int i=0;i<mat.size()-1;i++){
+            for(int j=i+1;j<mat.size();j++){
+                swap(mat[i][j],mat[j][i]);
+            }
+        }
+        return mat;
+    }
+};
