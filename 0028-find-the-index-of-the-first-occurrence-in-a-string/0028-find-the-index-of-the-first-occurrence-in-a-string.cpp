53bf9d60c4207ea6f18cdd755a5dcc3c9aa83f26
ABHAY KUMAWAT <abhaykumawat227@gmail.com>
2026-06-28 02:09:49 +0530

    Time: 2 ms (23.83%), Space: 9.3 MB (26.16%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,23 +1,59 @@
 class Solution {
 public:
-    int strStr(string haystack, string needle) {
-        int ans = -1;
-        for(int i=0;i<=haystack.size();i++){
-            int second = 0, first = i;
-            bool pre = 0;
-            while(second<needle.size()){
-                if(haystack[first]==needle[second])
+    void lpsfind(vector<int>&lps,string s){
+        int first = 0, second = 1;
+        while(second<s.size()){
+            if(s[first]==s[second]){
+                lps[second] = first+1;
                 first++,second++;
+            }
+            else{
+                if(first==0){
+                lps[second] = 0;
+                second++;
+                }
                 else{
-                    pre = 1;
-                    break;
+                    first = lps[first-1];
                 }
             }
-            if(!pre){
-                ans = i;
-                break;
+        }
+    }
+    int strStr(string haystack, string needle) {
+        vector<int>lps(needle.size(),0);
+        lpsfind(lps,needle);
+        int first = 0, second = 0;
+        while(second<needle.size() && first<haystack.size()){
+            if(needle[second]==haystack[first])
+            first++,second++;
+            else{
+                if(second==0)
+                first++;
+                else{
+                    second = lps[second-1];
+                }
             }
         }
-         return ans;
+        if(second==needle.size())
+        return first-second;
+        else
+        return -1;
+        // int ans = -1;
+        // for(int i=0;i<=haystack.size();i++){
+        //     int second = 0, first = i;
+        //     bool pre = 0;
+        //     while(second<needle.size()){
+        //         if(haystack[first]==needle[second])
+        //         first++,second++;
+        //         else{
+        //             pre = 1;
+        //             break;
+        //         }
+        //     }
+        //     if(!pre){
+        //         ans = i;
+        //         break;
+        //     }
+        // }
+        //  return ans;
     }
 };
