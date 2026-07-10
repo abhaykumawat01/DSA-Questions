8225343c764c0833b65bee0d6e89b8673da625a6
ABHAY KUMAWAT <abhaykumawat227@gmail.com>
2026-07-10 06:29:00 +0530

    Time: 47 ms (6.44%), Space: 43.1 MB (5.01%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if(numRows==1){
+            return s;
+        }
+        vector<vector<char>>ans(numRows,vector<char>(s.size(),'#'));
+        int i = 0;
+        int row = 0;
+        int col = 0;
+        while(i<s.size()){
+            if(col%(numRows-1)==0 && row<numRows){
+                ans[row][col] = s[i];
+                i++,row++;
+            }
+            else if(row==numRows){
+                int j = row-2;
+                col++;
+                while(j && i<s.size()){
+                    ans[j][col] = s[i];
+                    j--,col++,i++;
+                }
+                row = j;
+            }
+        }
+        string a = "";
+        for(int i=0;i<numRows;i++){
+            for(int j=0;j<=col;j++){
+                if(ans[i][j]!='#')
+                a +=ans[i][j];
+            }
+        }
+        return a;
+    }
+};
