ff81e5cd3900b75723a63355f4c8008d2e7bd65a
ABHAY KUMAWAT <abhaykumawat227@gmail.com>
2026-07-13 20:28:37 +0530

    Time: 3 ms (9.11%), Space: 8.8 MB (89.46%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>ans;
+        for(int i=0;i<s.size();i++){
+            //stack is empty
+            if(ans.empty())
+            ans.push(s[i]);
+            else if(ans.top()=='('){
+                if(s[i]==')')
+                ans.pop();
+                else
+                ans.push(s[i]);
+            }
+            else if(ans.top()=='['){
+                if(s[i]==']')
+                ans.pop();
+                else
+                ans.push(s[i]);
+            }
+            else if(ans.top()=='{'){
+                if(s[i]=='}')
+                ans.pop();
+                else ans.push(s[i]);
+            }
+            else
+            ans.push(s[i]);
+        }
+        return ans.empty();
+    }
+};
