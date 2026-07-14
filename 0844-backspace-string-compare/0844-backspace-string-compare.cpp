435fd24c600fee828a643f46cd6c854baa85afec
ABHAY KUMAWAT <abhaykumawat227@gmail.com>
2026-07-14 08:46:57 +0530

    Time: 0 ms (100%), Space: 8.6 MB (46.31%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        stack<char>s1;
+        stack<char>s2;
+        for(int i=0;i<s.size();i++){
+            if(s1.empty() && s[i]!='#')
+            s1.push(s[i]);
+            else if(s1.empty() && s[i]=='#')
+            continue;
+            else if(s[i]=='#')
+            s1.pop();
+            else
+            s1.push(s[i]);
+        }
+        for(int i=0;i<t.size();i++){
+            if(s2.empty() && t[i]!='#')
+            s2.push(t[i]);
+            else if(s2.empty() && t[i]=='#')
+            continue;
+            else if(t[i]=='#')
+            s2.pop();
+            else
+            s2.push(t[i]);
+        }
+        while(!s1.empty() && !s2.empty()){
+            if(s1.top()!=s2.top())
+            return 0;
+            else{
+                s1.pop();
+                s2.pop();
+            }
+        }
+        if(!s1.empty() || !s2.empty())
+        return 0;
+        else
+        return 1;
+
+
+    }
+};
